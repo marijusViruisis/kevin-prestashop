de5107b25c234dedea41e6ca6d0e2ef3dbb9b99b
Tomas Mačiulis <tomas.maciulis@kevin.eu>
2022-07-08 15:29:07 +0300

    TS-400: bump version

--- kevin.php
+++ kevin.php
@@ -46,7 +46,7 @@ class Kevin extends PaymentModule
     {
         $this->name = 'kevin';
         $this->tab = 'payments_gateways';
-        $this->version = '1.9.3';
+        $this->version = '1.9.4';
         $this->ps_versions_compliancy = ['min' => '1.6', 'max' => _PS_VERSION_];
         $this->author = 'kevin.';
         $this->controllers = ['redirect', 'confirm', 'webhook'];

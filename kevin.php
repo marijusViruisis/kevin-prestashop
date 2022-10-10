53661dad228562ff86c34cffffe33455637ba126
jakubmilewski <jakub.milewski@kevin.eu>
2022-10-10 17:30:43 +0200

    Fix check custom env exists

--- kevin.php
+++ kevin.php
@@ -46,7 +46,7 @@ class Kevin extends PaymentModule
     {
         $this->name = 'kevin';
         $this->tab = 'payments_gateways';
-        $this->version = '1.10.4';
+        $this->version = '1.10.6';
         $this->ps_versions_compliancy = ['min' => '1.6', 'max' => _PS_VERSION_];
         $this->author = 'kevin.';
         $this->controllers = ['redirect', 'confirm', 'webhook'];
@@ -742,7 +742,7 @@ class Kevin extends PaymentModule
         ];
         $options = array_merge($options, $this->getSystemData());
 
-        if (API_KEVIN_DOMAIN) {
+        if (defined('API_KEVIN_DOMAIN')) {
             $options['domain'] = API_KEVIN_DOMAIN;
         }
 
@@ -1010,7 +1010,7 @@ class Kevin extends PaymentModule
     {
         $link = $this->context->link->getModuleLink($module, $controller, $params, true);
 
-        if (CUSTOM_WEBHOOK_URL) {
+        if (defined('CUSTOM_WEBHOOK_URL')) {
             $link = str_replace($this->context->link->getBaseLink(), CUSTOM_WEBHOOK_URL, $link);
         }
 
